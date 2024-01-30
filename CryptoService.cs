f3888b3aa9611260b0b3ef026539b8d2db3e0488
thomasv7877 <tvandenhauwe@gmail.com>
2024-01-30 15:21:38 +0100

    first commit

---
+++ CryptoService.cs
@@ -0,0 +1,85 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+
+namespace dotnet_crypto;
+
+public static class CryptoService
+{
+public static void GenerateKeys(int keyLength, out RSAParameters publicKey, out RSAParameters privateKey)
+{
+    using (var rsa = RSA.Create())
+    {
+        rsa.KeySize = keyLength;
+        publicKey = rsa.ExportParameters(includePrivateParameters: false);
+        privateKey = rsa.ExportParameters(includePrivateParameters: true);
+    }
+}
+public static void ExportKeys(RSAParameters privateKey, RSAParameters publicKey, string path = ""){
+    using (var rsa = RSA.Create())
+    {
+        // rsa.KeySize = keyLength;
+        rsa.ImportParameters(privateKey);
+        // rsa.ImportParameters(publicKey);
+
+        string privateKeyXml = rsa.ToXmlString(includePrivateParameters: true);
+        File.WriteAllText("private.key", privateKeyXml);
+        // string publicKeyXml = rsa.ToXmlString(includePrivateParameters: false);
+        // File.WriteAllText("public.key", publicKeyXml);
+    }
+}
+public static void ImportKeys(out RSAParameters publicKey, out RSAParameters privateKey, string path = ""){
+    string privateKeyXml = File.ReadAllText("private.key");
+    // string publicKeyXml = File.ReadAllText("public.key");
+
+    using (var rsa = RSA.Create())
+    {
+        // rsa.KeySize = keyLength;
+        // rsa.FromXmlString(publicKeyXml);
+        rsa.FromXmlString(privateKeyXml);
+        publicKey = rsa.ExportParameters(includePrivateParameters: false);
+        privateKey = rsa.ExportParameters(includePrivateParameters: true);
+    }
+}
+
+
+// public static void ImportKeys(int keyLength, out RSAParameters publicKey, out RSAParameters privateKey, string path = ""){
+//     string privateKeyAsString = File.ReadAllText(Path.Join(path, "private.key"));
+//     string publicKeyAsString = File.ReadAllText(Path.Join(path, "public.key"));
+//     RSAParameters privateKeyParam = JsonSerializer.Deserialize<RSAParameters>(privateKeyAsString);
+//     RSAParameters publicKeyParam = JsonSerializer.Deserialize<RSAParameters>(publicKeyAsString);
+
+//     using (var rsa = RSA.Create())
+//     {
+//         rsa.KeySize = keyLength;
+//         rsa.ImportParameters(privateKeyParam);
+//         rsa.ImportParameters(publicKeyParam);
+//         publicKey = rsa.ExportParameters(includePrivateParameters: false);
+//         privateKey = rsa.ExportParameters(includePrivateParameters: true);
+//     }
+// }
+// public static void ExportKeys(RSAParameters privateKey, RSAParameters publicKey, string path = ""){
+//     var privateKeyJson = JsonSerializer.Serialize(privateKey);
+//     var publicKeyJson = JsonSerializer.Serialize(publicKey);
+//     File.WriteAllText(Path.Join(path, "private.key"), privateKeyJson);
+//     File.WriteAllText(Path.Join(path, "public.key"), publicKeyJson);
+// }
+public static byte[] Encrypt(byte[] data, RSAParameters publicKey)
+{
+    using (var rsa = RSA.Create())
+    {
+        rsa.ImportParameters(publicKey);
+        var result = rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
+        return result;
+    }
+}
+public static byte[] Decrypt(byte[] data, RSAParameters privateKey)
+{
+    using (var rsa = RSA.Create())
+    {
+        rsa.ImportParameters(privateKey);
+        return rsa.Decrypt(data, RSAEncryptionPadding.OaepSHA256);
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Text;
+using dotnet_crypto;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // var configPath = Path.Join(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config");
+        // Console.WriteLine(configPath);
+
+        var dataStr = "This is corporate research! Dont read me!";
+        var data = Encoding.UTF8.GetBytes(dataStr);
+        var keyLength = 2048; // size in bits
+        CryptoService.GenerateKeys(keyLength, out var publicKey, out var privateKey);
+        CryptoService.ExportKeys(privateKey, publicKey);
+        CryptoService.ImportKeys(out var readPublicKey, out var readPrivateKey);
+        // Console.WriteLine(readPrivateKey.ToString());
+
+        var encryptedData = CryptoService.Encrypt(data, readPublicKey);
+        var encryptedDataAsString = Convert.ToHexString(encryptedData);
+        Console.WriteLine("Encrypted Value:\n" + encryptedDataAsString);
+
+        var decryptedData = CryptoService.Decrypt(encryptedData, readPrivateKey);
+        var decryptedDataAsString = Encoding.UTF8.GetString(decryptedData);
+        Console.WriteLine(decryptedDataAsString);
+    }
+}
